c4f3d209bff1d0d2a2aaa43f9b350deb72b003f5
Muchiri-cmd <davismuchiri21@gmail.com>
2025-06-17 22:19:53 +0300

    Fix error handler

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
-export const errorHandler = (error: any, _req: Request, res: Response) => {
+export const errorHandler = (error: any, _req: Request, res: Response,_next:NextFunction) => {
   // console.log(error)
   res.status(error.status || 500).json({
     message: error.message || "Something wen wrong",
